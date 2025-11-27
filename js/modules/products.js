38a02cec04a8450557944e0ba8a50b5ae3c1414f
MatveyZZ <matveyka.sokolov.2004@mail.ru>
2025-11-27 15:47:40 +0300

    Update

--- js/modules/products.js
+++ js/modules/products.js
@@ -3,40 +3,211 @@ const Products = {
     data: [
         {
             id: 1,
-            name: "Пельмени Сибирские",
-            description: "Сибирские пельмени ручной работы, 1кг.",
-            price: 450,
-            image: "./assets/images/products/pelmeni.jpg",
-            category: "пельмени",
+            name: "Пельмени Сибирские", /////////////////////////
+            description: "Свинина, Говядина, 1кг.",
+            price: 450, /////////////////////////////////////
+            image: "./assets/images/products/sibirskie.jpg", ////////////////
+            category: "пельмени", ///////////////////////////////////
             tags: ["ручная работа", "сибирские", "премиум"]
         },
         {
             id: 2,
+            name: "Пельмени Русские", //
+            description: "Свинина, 0.4кг.",
+            price: 210, //
+            image: "./assets/images/products/russkie.jpg", //////////////////
+            category: "пельмени", //
+            tags: ["ручная работа", "сибирские", "премиум"]
+        },
+        {
+            id: 3,
+            name: "Пельмени Даниловские", //
+            description: "Свинина, 0.4кг.",
+            price: 200,
+            image: "./assets/images/products/danil.jpg", ///////////////////////
+            category: "пельмени",
+            tags: ["ручная работа", "сибирские", "премиум"]
+        },
+        {
+            id: 4,
+            name: "Пельмени Крестьянские",
+            description: "Свинина, 0.5кг.",
+            price: 185,
+            image: "./assets/images/products/krest.jpg", ///////////////////
+            category: "пельмени",
+            tags: ["ручная работа", "сибирские", "премиум"]
+        },
+        {
+            id: 5,
+            name: "Пельмени Столичные",
+            description: "Свинина, 0.5кг.",
+            price: 250,
+            image: "./assets/images/products/stol.jpg", ////////////////////
+            category: "пельмени",
+            tags: ["ручная работа", "сибирские", "премиум"]
+        },
+        {
+            id: 6,
             name: "Котлеты 'Киевские'",
-            description: "Нежнейшие котлеты из куриной грудки, 800г",
+            description: "Курица, 0.8кг",
             price: 380,
-            image: "./assets/images/products/kievskie.jpg",
+            image: "./assets/images/products/kievskie.jpg", //////////////////
             category: "котлеты",
             tags: ["куриные", "нежные", "классические"]
         },
         {
-            id: 3,
+            id: 7,
             name: "Зразы с сыром",
-            description: "Куриные котлеты с сыром, 800г",
+            description: "Курица, 0.8кг",
             price: 420,
-            image: "./assets/images/products/zrazy.jpg",
-            category: "зразы",
+            image: "./assets/images/products/zrazy.jpg", ///////////////////////
+            category: "котлеты",
             tags: ["с сыром", "куриные", "сытные"]
         },
         {
-            id: 4,
+            id: 8,
             name: "Котлеты 'Домашние'",
-            description: "Котлеты из свинины, 600г",
+            description: "Свинина, 0.6кг",
             price: 300,
-            image: "./assets/images/products/domashnie.jpg",
+            image: "./assets/images/products/domashnie.jpg", //////////////////
             category: "котлеты",
             tags: ["свиные", "домашние", "традиционные"]
-        }
+        },
+        {
+            id: 9,
+            name: "Ромштекс",
+            description: "Свинина, 0.6кг",
+            price: 275,
+            image: "./assets/images/products/romsh.jpg",
+            category: "котлеты",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 10,
+            name: "Котлеты Московские",
+            description: "Свинина, 0.6кг",
+            price: 260,
+            image: "./assets/images/products/moskva.jpg",
+            category: "котлеты",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 11,
+            name: "Наггетсы",
+            description: "Курица, 0.8кг",
+            price: 380,
+            image: "./assets/images/products/naggets.jpg",
+            category: "наггетсы",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 12,
+            name: "Люля-Кебаб",
+            description: "Говядина, 0.8кг",
+            price: 380,
+            image: "./assets/images/products/lula.jpg",
+            category: "котлеты",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 13,
+            name: "Шницель",
+            description: "Свинина, 0.6кг",
+            price: 300,
+            image: "./assets/images/products/shnisel.jpg",
+            category: "котлеты",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 14,
+            name: "Фрикадельки",
+            description: "Говядина, 0.5кг",
+            price: 280,
+            image: "./assets/images/products/frik.jpg",
+            category: "тефтели",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 15,
+            name: "Ёжики",
+            description: "Свинина, 0.5кг",
+            price: 280,
+            image: "./assets/images/products/ezhik.jpg",
+            category: "тефтели",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 16,
+            name: "Котлеты Аппетитные",
+            description: "Свинина, 0.8кг",
+            price: 275,
+            image: "./assets/images/products/appetit.jpg",
+            category: "котлеты",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 17,
+            name: "Котлеты Деревенские",
+            description: "Курица, 0.6кг",
+            price: 230,
+            image: "./assets/images/products/derev.jpg",
+            category: "котлеты",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 18,
+            name: "Котлеты Нежные",
+            description: "Курица, 0.6кг",
+            price: 250,
+            image: "./assets/images/products/nezhnue.jpg",
+            category: "котлеты",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 19,
+            name: "Тефтели Ароматные",
+            description: "Свинина, 0.5кг",
+            price: 280,
+            image: "./assets/images/products/teftel.jpg",
+            category: "тефтели",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 20,
+            name: "Колбаски Аппетитки",
+            description: "Свинина, 0.8кг",
+            price: 520,
+            image: "./assets/images/products/kolbas.jpg",
+            category: "колбаски",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 21,
+            name: "Купаты Восточные",
+            description: "Курица, 0.8кг",
+            price: 520,
+            image: "./assets/images/products/vostok.jpg",
+            category: "колбаски",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 22,
+            name: "Биточки Особые",
+            description: "Курица, 0.5кг",
+            price: 280,
+            image: "./assets/images/products/bit.jpg",
+            category: "котлеты",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
+        {
+            id: 23,
+            name: "Бифштекс",
+            description: "Говядина, 0.6кг",
+            price: 325,
+            image: "./assets/images/products/bif.jpg",
+            category: "котлеты",
+            tags: ["свиные", "домашние", "традиционные"]
+        },
     ],
 
     // Получение товара по ID
